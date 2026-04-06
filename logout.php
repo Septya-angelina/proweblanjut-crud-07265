a1be12cc20d11742a23dad66e84d4a471103b025
Septya-angelina <angelinaseptya73@gmail.com>
2026-04-06 21:20:00 +0700

    Menambahkan fitur logout dengan menghapus session pengguna

---
+++ logout.php
@@ -0,0 +1,15 @@
+<?php
+session_start();
+include "koneksi.php";
+
+if (isset($_SESSION["user_id"])) {
+    $stmt = $conn->prepare("UPDATE users SET remember_token=NULL WHERE id=?");
+    $stmt->bind_param("i", $_SESSION["user_id"]);
+    $stmt->execute();
+}
+
+session_destroy();
+setcookie("remember_token", "", time() - 3600, "/");
+
+header("Location: login.php");
+exit();
