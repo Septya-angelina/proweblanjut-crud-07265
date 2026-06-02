689d7941f9b651eb9dcf27193d2644893775c4c5
Septya-angelina <angelinaseptya73@gmail.com>
2026-06-02 16:43:11 +0700

    Menambahkan koneksi database menggunakan metode PDO

--- koneksi.php
+++ koneksi.php
@@ -4,19 +4,16 @@ $user = 'root';
 $pass = '';
 $dbname = 'pwl_07265';
 
-$conn = new PDO(
+$pdo = new PDO(
     dsn: "mysql:host=$host;dbname=$dbname",
     username:"$user",
     password: "$pass"
 );
-if (!$conn){
+if (!$pdo){
     die("koneksi gagal");
 }
-/*
 $conn = mysqli_connect(hostname: $host, username: $user, password: $pass, database: $dbname);
 if ($conn->connect_error){
     die("koneksi tak berhasil" . $conn->connect_error);
-}else{
-    die("koneksi berhasil");
-}*/
+}
 ?>
